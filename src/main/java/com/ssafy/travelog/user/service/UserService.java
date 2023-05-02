699f5b0f34dd3abd6367b503a74b45a95b3c11b6
mina <mineezm9@gmail.com>
2023-05-02 14:19:21 +0900

    refactor: UserService 메서드 파라미터 타입 변경
    modifyUser, deleteUser의 파라미터 타입을 UserDto에서 Map<String, String>으로 변경

--- src/main/java/com/ssafy/travelog/user/service/UserService.java
+++ src/main/java/com/ssafy/travelog/user/service/UserService.java
@@ -4,9 +4,11 @@ import com.ssafy.travelog.user.dto.UserDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public interface UserService {
-    int modifyUser(UserDto user);
-    int deleteUser(UserDto user);
+    int modifyUser(Map<String, String> param);
+    int deleteUser(Map<String, String> param);
 }
