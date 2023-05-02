27005c0f65f5d3a5797fbbe904daf111bab97608
mina <mineezm9@gmail.com>
2023-05-02 14:19:54 +0900

    feat: UserDao 인터페이스 구현

---
+++ src/main/java/com/ssafy/travelog/user/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.ssafy.travelog.user.dao;
+
+import com.ssafy.travelog.user.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+@Mapper
+public interface UserDao {
+    int modifyUser(UserDto user) throws SQLException;
+    int deleteUser(Map<String, String> param) throws SQLException;
+}
