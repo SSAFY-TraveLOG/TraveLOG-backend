1c9f6a64a1c749b8e2c99282923571cb78ad4245
mina <mineezm9@gmail.com>
2023-05-02 14:46:50 +0900

    fix: SQLException 처리

--- src/main/java/com/ssafy/travelog/user/service/UserService.java
+++ src/main/java/com/ssafy/travelog/user/service/UserService.java
@@ -4,11 +4,12 @@ import com.ssafy.travelog.user.dto.UserDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public interface UserService {
-    int modifyUser(Map<String, String> param);
-    int deleteUser(Map<String, String> param);
+    int modifyUser(Map<String, String> param) throws SQLException;
+    int deleteUser(Map<String, String> param) throws SQLException;
 }
