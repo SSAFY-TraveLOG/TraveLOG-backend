9550d5ba77cd352e794a714bf46fd1caf09e554c
mina <mineezm9@gmail.com>
2023-05-03 15:53:01 +0900

    feat: Auth Dao, Mapper 구현

---
+++ src/main/java/com/ssafy/travelog/auth/dao/AuthDao.java
@@ -0,0 +1,12 @@
+package com.ssafy.travelog.auth.dao;
+
+import com.ssafy.travelog.auth.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+@Mapper
+public interface AuthDao {
+    
+}
