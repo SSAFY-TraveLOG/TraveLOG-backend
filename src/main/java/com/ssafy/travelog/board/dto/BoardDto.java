93dae9dc7d1846022677f9637ca76741f1bc01f2
mina <mineezm9@gmail.com>
2023-05-03 17:19:54 +0900

    feat: BoardDto 구현

---
+++ src/main/java/com/ssafy/travelog/board/dto/BoardDto.java
@@ -0,0 +1,72 @@
+package com.ssafy.travelog.board.dto;
+
+public class BoardDto {
+    private int articleNo;
+    private int userNo;
+
+    private String subject;
+
+    private String content;
+
+    private int readCount;
+
+    private String registerTime;
+
+    private String modifiedTime;
+
+    public int getArticleNo() {
+        return articleNo;
+    }
+
+    public void setArticleNo(int articleNo) {
+        this.articleNo = articleNo;
+    }
+
+    public int getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getReadCount() {
+        return readCount;
+    }
+
+    public void setReadCount(int readCount) {
+        this.readCount = readCount;
+    }
+
+    public String getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(String modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+}
