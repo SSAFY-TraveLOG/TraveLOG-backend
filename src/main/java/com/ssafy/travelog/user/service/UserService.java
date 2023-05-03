3c8d00c0bc3a6ae583d77fb9abe512d5952c1f9c
mina <mineezm9@gmail.com>
2023-05-03 16:24:19 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/com/ssafy/travelog/user/service/UserService.java
+++ src/main/java/com/ssafy/travelog/user/service/UserService.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.SQLException;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/user")
 public interface UserService {
     int modifyUser(Map<String, String> param) throws SQLException;
     int deleteUser(Map<String, String> param) throws SQLException;
