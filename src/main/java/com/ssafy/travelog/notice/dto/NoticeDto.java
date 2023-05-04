b12763e717ba8841065557147ba0e0e8fdd4b7fe
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-05-04 10:48:44 +0900

    fix: NoticeDto 필드 제한자 수정

--- src/main/java/com/ssafy/travelog/notice/dto/NoticeDto.java
+++ src/main/java/com/ssafy/travelog/notice/dto/NoticeDto.java
@@ -1,49 +1,51 @@
 package com.ssafy.travelog.notice.dto;
 
-public class NoticeDto {
-    static int noticeNo;
-    static String subject;
-    static String content;
-    static String registerTime;
-    static String modifiedTime;
+import jdk.nashorn.internal.objects.annotations.Getter;
 
-    public static int getNoticeNo() {
+public class NoticeDto {
+    private int noticeNo;
+    private String subject;
+    private String content;
+    private String registerTime;
+    private String modifiedTime;
+
+    public int getNoticeNo() {
         return noticeNo;
     }
 
-    public static void setNoticeNo(int noticeNo) {
-        NoticeDto.noticeNo = noticeNo;
+    public void setNoticeNo(int noticeNo) {
+        this.noticeNo = noticeNo;
     }
 
-    public static String getSubject() {
+    public String getSubject() {
         return subject;
     }
 
-    public static void setSubject(String subject) {
-        NoticeDto.subject = subject;
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
-    public static String getContent() {
+    public String getContent() {
         return content;
     }
 
-    public static void setContent(String content) {
-        NoticeDto.content = content;
+    public void setContent(String content) {
+        this.content = content;
     }
 
-    public static String getRegisterTime() {
+    public String getRegisterTime() {
         return registerTime;
     }
 
-    public static void setRegisterTime(String registerTime) {
-        NoticeDto.registerTime = registerTime;
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
     }
 
-    public static String getModifiedTime() {
+    public String getModifiedTime() {
         return modifiedTime;
     }
 
-    public static void setModifiedTime(String modifiedTime) {
-        NoticeDto.modifiedTime = modifiedTime;
+    public void setModifiedTime(String modifiedTime) {
+        this.modifiedTime = modifiedTime;
     }
 }
