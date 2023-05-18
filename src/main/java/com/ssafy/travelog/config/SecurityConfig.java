d0b65af29ea7a038b23cbb6b1903992be3b523c9
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-05-18 12:10:14 +0900

    fix: HttpMethod Option 권한 수정

--- src/main/java/com/ssafy/travelog/config/SecurityConfig.java
+++ src/main/java/com/ssafy/travelog/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.ssafy.travelog.util.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,6 +38,7 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**/**").permitAll()
                         .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
