dcda3e3470cf56cd16ca400ffce722395a5f5df6
mina <mineezm9@gmail.com>
2023-05-19 15:53:40 +0900

    fix: WebSecurity 설정
    Spring security에서 swagger-ui.html 무시하도록 WebSecurity 설정

--- src/main/java/com/ssafy/travelog/config/SecurityConfig.java
+++ src/main/java/com/ssafy/travelog/config/SecurityConfig.java
@@ -6,30 +6,41 @@ import com.ssafy.travelog.util.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
-    private final TokenProvider jwtTokenProvider;
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
 
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
-
-                http
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/favicon.ico")
+                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
+//                .antMatchers("/resources/**")
+//                .antMatchers("/css/**")
+//                .antMatchers("/vendor/**")
+//                .antMatchers("/js/**")
+//                .antMatchers("/favicon*/**")
+//                .antMatchers("/img/**")
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -37,22 +48,9 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                .antMatchers(HttpMethod.OPTIONS, "/**/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-
-//        http
-//                .httpBasic().disable() // rest api이므로 기본설정 미사용
-//                .csrf().disable() // rest api이므로 csrf 보안 미사용
-//                .formLogin().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt로 인증하므로 세션 미사용
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/auth/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // jwt 필터 추가
-
-        return http.build();
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
