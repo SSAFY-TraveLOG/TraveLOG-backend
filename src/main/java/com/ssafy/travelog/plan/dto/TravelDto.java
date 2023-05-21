142300bd0bb624e215ab9dba39baede2cc47336e
Mina Park <mineezm9@gmail.com>
2023-05-21 17:50:00 +0900

    feat: Travel Dto 구현

---
+++ src/main/java/com/ssafy/travelog/plan/dto/TravelDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.travelog.plan.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class TravelDto {
+    private String title;
+    private String description;
+
+    private String startDate;
+
+    private String endDate;
+
+    private String sidoName;
+}
