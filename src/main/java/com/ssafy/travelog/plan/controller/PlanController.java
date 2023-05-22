9f659bf9b9e9d76fede65d5b8c1aea50601d3bfb
Lee-Jiseung <ljss98@naver.com>
2023-05-22 13:40:30 +0900

    feat: 여행계획 컨트롤러 예외처리 추가

--- src/main/java/com/ssafy/travelog/plan/controller/PlanController.java
+++ src/main/java/com/ssafy/travelog/plan/controller/PlanController.java
@@ -1,11 +1,19 @@
 package com.ssafy.travelog.plan.controller;
 
 import com.ssafy.travelog.plan.service.PlanService;
+import com.ssafy.travelog.util.Message;
+import com.ssafy.travelog.util.StatusEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
 
 @RestController
 @RequestMapping("/plan")
@@ -16,5 +24,18 @@ public class PlanController {
     @Autowired
     public PlanController(PlanService planService) {
         this.planService = planService;
+    }
+
+    private ResponseEntity<Message> exceptionHandling(Exception e) {
+        e.printStackTrace();
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        message.setStatus(StatusEnum.NOT_FOUND);
+        message.setCode(StatusEnum.NOT_FOUND);
+        message.setMessage("요청에 실패하였습니다.");
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 }
